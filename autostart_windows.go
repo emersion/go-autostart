1da5780a5a617d165383e45ce85d8dab21f3d8f7
emersion <contact@emersion.fr>
2016-06-24 17:17:57 +0200

    Adds windows support

---
+++ autostart_windows.go
@@ -0,0 +1,39 @@
+package autostart
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var startupDir string
+
+func init() {
+	startupDir = filepath.Join(os.Getenv("USERPROFILE"), "Start Menu", "Programs", "Startup")
+}
+
+func (a *App) path() string {
+	return filepath.Join(startupDir, a.Name + ".bat")
+}
+
+func (a *App) IsEnabled() bool {
+	_, err := os.Stat(a.path())
+	return !os.IsNotExist(err)
+}
+
+func (a *App) Enable() error {
+	s := `"` + strings.Join(a.Exec, `" "`) + `"` + "\r\n"
+
+	f, err := os.Create(a.path())
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err := f.Write([]byte(s))
+	return err
+}
+
+func (a *App) Disable() error {
+	return os.Remove(a.path())
+}
