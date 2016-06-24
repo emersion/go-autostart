4f7f49c0209162a5f1af5c937abeabe689c31018
emersion <contact@emersion.fr>
2016-06-24 17:06:24 +0200

    Adds Linux & BSD support

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # go-autostart
-A Go library to run a command after login
+
+A Go library to run a command after login.
+
+## Usage
+
+```go
+package main
+
+import (
+	"log"
+	"github.com/ProtonMail/go-autostart"
+)
+
+func main() {
+	app := &autostart.App{
+		Name: "test",
+		DisplayName: "Just a Test App",
+		Exec: []string{"bash", "-c", "echo autostart >> ~/autostart.txt"},
+	}
+
+	if app.IsEnabled() {
+		log.Println("App is already enabled, removing it...")
+
+		if err := app.Disable(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Println("Enabling app...")
+
+		if err := app.Enable(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Println("Done!")
+}
+```
+
+## Behavior
+
+* On Linux and BSD, it creates a `.desktop` file in `$XDG_CONFIG_HOME/autostart`
+  (i.e. `$HOME/.config/autostart`). See http://askubuntu.com/questions/48321/how-do-i-start-applications-automatically-on-login
+* On macOS, it creates and enables a `launchd` job. See http://blog.gordn.org/2015/03/implementing-run-on-login-for-your-node.html
+* On Windows, it creates a `.bat` file in `%USERPROFILE%\Start Menu\Programs\Startup`
+
+## License
+
+MIT
---
+++ autostart_xdg.go
@@ -0,0 +1,66 @@
+// +build !windows,!darwin
+
+package autostart
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+const desktopTemplate = `[Desktop Entry]
+Type=Application
+Name={{.DisplayName}}
+Exec={{.Exec}}
+{{- if .Icon}}Icon={{.Icon}}{{end}}
+X-GNOME-Autostart-enabled=true
+`
+
+var autostartDir string
+
+func init() {
+	if os.Getenv("XDG_CONFIG_HOME") != "" {
+		autostartDir = os.Getenv("XDG_CONFIG_HOME")
+	} else {
+		autostartDir = filepath.Join(os.Getenv("HOME"), ".config")
+	}
+	autostartDir = filepath.Join(autostartDir, "autostart")
+}
+
+func (a *App) path() string {
+	return filepath.Join(autostartDir, a.Name + ".desktop")
+}
+
+// Check if the app is enabled on startup.
+func (a *App) IsEnabled() bool {
+	_, err := os.Stat(a.path())
+	return !os.IsNotExist(err)
+}
+
+type app struct {
+	*App
+}
+
+// Override App.Exec to return a string.
+func (a *app) Exec() string {
+	return `"` + strings.Join(a.App.Exec, `" "`) + `"`
+}
+
+// Enable this app on startup.
+func (a *App) Enable() error {
+	t := template.Must(template.New("desktop").Parse(desktopTemplate))
+
+	f, err := os.Create(a.path())
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return t.Execute(f, &app{a})
+}
+
+// Disable this app on startup.
+func (a *App) Disable() error {
+	return os.Remove(a.path())
+}
